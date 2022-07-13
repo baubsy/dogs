1bd4b6511bcc557de524ee642e6233709d59a48f
Baubs <pathogenic@gmail.com>
2022-07-13 18:16:12 -0500

    hooking up combo box

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,8 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import dogsAPI from '../dogsAPI';
+import { Autocomplete, TextField } from '@mui/material';
+
+interface IBreed {
+  name: string;
+}
 
 const Main = () => {
-    const [breeds, setBreeds] = useState([]);
+    const [breeds, setBreeds] = useState<IBreed[]>([]);
+    const [selectedBreeds, setSelectedBreeds] = useState<IBreed[]>([]);
     
     useEffect(() => {
       getBreeds();
@@ -16,13 +22,28 @@ const Main = () => {
     }
 
     const logBreeds = () => {
-      console.log(breeds);
+      console.log(selectedBreeds);
     }
   return (
     <div>
       <h1>setup</h1>
+      <button onClick={logBreeds}>log</button>
+      <Autocomplete 
+        multiple 
+        id="breeds" 
+        options={breeds}
+        getOptionLabel={(option => option.name)} 
+        onChange = {(event, value) => setSelectedBreeds(value)}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            variant="standard"
+            label="Select Breeds to Compare"
+          />
+        )}
+      />
     </div>
   )
-}
+};
 
 export default Main;

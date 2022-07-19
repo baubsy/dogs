e46e6f99f4dbd4446c23e95756dd8aaa4e435a15
Baubs <pathogenic@gmail.com>
2022-07-18 21:38:23 -0500

    playing with d3

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -1,15 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 import IBreed from '../dogBreed';
+import * as d3 from "d3";
 
 interface GraphProps {
     compare: string;
     breeds: IBreed[];
 }
+
 const Graph = (props: GraphProps) => {
+
+  const svg = d3
+    .select("#bar-graph")
+    .append("svg")
+    .attr("width", 500)
+    .attr("height", 500);
+  const handleClick = () => {
+    
+
+    svg
+    .selectAll("rect")
+    .data(props.breeds)
+    .enter()
+      .append("rect")
+      .attr("width", 50)
+      .attr("height", 23)
+      .attr("x", (d,i) => i * 60);
+  }
+  
   return (
     <div>
         <h1>Graph</h1>
         <h1>{props.compare}</h1>
+        <button onClick={handleClick}>Graph test</button>
+        <div id="bar-graph"/>
     </div>
   )
 }

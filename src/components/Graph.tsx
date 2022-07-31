d3096519e04a2006d42b2276fd753d6dcad303a7
Baubs <pathogenic@gmail.com>
2022-07-31 18:01:14 -0500

    Graph and minor layout

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -1,6 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import IBreed from '../dogBreed';
 import * as d3 from "d3";
+import BarChart from '../barChart';
+import { Grid, Box } from "@mui/material";
 
 interface GraphProps {
     compare: string,
@@ -10,6 +12,8 @@ interface GraphProps {
 
 const Graph = (props: GraphProps) => {
   const [modBreeds, setModBreeds] = useState<IBreed[]>([]);
+  const [breedChart, setBreedChart] = useState<any>();
+  const svgRef = useRef(null);
 
   useEffect(() => {
     const moddedBreeds = props.breeds.map(breed => {
@@ -25,12 +29,26 @@ const Graph = (props: GraphProps) => {
       return retBreed
     })
     setModBreeds(moddedBreeds);
-  }, [props.breeds])
-  const svg = d3
-    .select("#bar-graph")
-    .append("svg")
-    .attr("width", 500)
-    .attr("height", 500);
+  }, [props.breeds]);
+  useEffect(() => {
+    if(modBreeds.length > 0){
+      const chart = BarChart(modBreeds, {
+        x: d => d.name,
+        y: d => d.avg_weight,
+        xDomain: d3.groupSort(modBreeds, ([d]) => d.avg_weight, d => d.name),
+        yFormat: "f",
+        yLabel: "Pounds",
+        width: 500,
+        height: 500,
+        color: "steelblue",
+        title: "Dog Breeds",
+        yDomain: undefined
+        
+      })
+      setBreedChart(chart);
+    }
+  }, [modBreeds])
+  
 
   const apiAvg = (str: string) => {
     if(!str.includes('-')){
@@ -43,23 +61,21 @@ const Graph = (props: GraphProps) => {
   const handleClick = () => {
     
     console.log(modBreeds);
-    svg
-    .selectAll("rect")
-    .data(modBreeds)
-    .enter()
-      .append("rect")
-      .attr("width", 50)
-      .attr("height", 23)
-      .attr("x", (d,i) => i * 60);
+    //const svg = d3.select(svgRef.current).node().appendChild(breedChart);
+    const svg = d3.select(svgRef.current);
+    svg.selectAll("*").remove();
+    svg.node().appendChild(breedChart);
   }
   
   return (
-    <div>
+    <Grid container alignItems="center" justifyContent="center">
         <h1>Graph</h1>
         <h1>{props.compare}</h1>
         <button onClick={handleClick}>Graph test</button>
-        <div id="bar-graph"/>
-    </div>
+        <Grid container item xs={12} alignItems="center" justifyContent="center">
+          <div ref={svgRef}/>
+        </Grid>        
+    </Grid>
   )
 }
 
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,24 +1,43 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Selection from "./Selection";
 import IBreed from "../dogBreed";
 import Graph from "./Graph";
+import { Grid, Box, Container } from "@mui/material";
 
 const Main = () => {
     const [selectedBreeds, setSelectedBreeds] = useState<IBreed[]>([]);
     const [compare, setCompare] = useState("weight");
     const [units, setUnits] = useState("imperial");
-  
-    const logbreeds  = () => {
-      console.log(selectedBreeds);
-      console.log(compare);
-    }
+
+    const logbreeds = () => {
+        console.log(selectedBreeds);
+        console.log(compare);
+    };
     return (
-        <div>
-            <button onClick={logbreeds}>test</button>
-            <Selection setSelectedBreeds={setSelectedBreeds} compare={compare} setCompare={setCompare}/>
-            <Graph compare={compare} breeds={selectedBreeds} units={units}/>
-        </div>
-    )
+        <Container>
+            <Box>
+                <Grid container justifyContent="center" alignItems="center">
+                    <Grid item xs={4}>
+                        <button onClick={logbreeds}>test</button>
+                    </Grid>
+                    <Grid item xs={8}>
+                        <Selection
+                            setSelectedBreeds={setSelectedBreeds}
+                            compare={compare}
+                            setCompare={setCompare}
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Graph
+                            compare={compare}
+                            breeds={selectedBreeds}
+                            units={units}
+                        />
+                    </Grid>
+                </Grid>
+            </Box>
+        </Container>
+    );
 };
 
 export default Main;

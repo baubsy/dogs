34105923fcb7660453ae8a43c81945fa6a20c306
Baubs <pathogenic@gmail.com>
2022-08-30 23:12:35 -0500

    styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,9 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <h1>footer</h1>
+    <div className="header">
+        <p>Made by Robert Swyers. <a href="https://github.com/baubsy/dogs">Github Repo</a></p>
+    </div>
   )
 }
 

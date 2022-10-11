f41b90a2df52e58347bedb40d1bd7dda07b506ff
Baubs <pathogenic@gmail.com>
2022-10-11 00:19:43 -0500

    Footer work

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 
 const Footer = () => {
-  return (
-    <div>
-        <p>Made by Robert Swyers. <a href="https://github.com/baubsy/dogs">Github Repo</a></p>
-    </div>
-  )
-}
+    return (
+        <div className="footer">
+            <p>
+                Made by Robert Swyers using data from <a href="https://thedogapi.com/">The Dog API</a>.
+                <br/>
+                <a href="https://github.com/baubsy/dogs">Github Repo</a>
+                <br/>
+                <a href="https://www.linkedin.com/in/robert-swyers/">LinkedIn</a>
+            </p>
+        </div>
+    );
+};
 
 export default Footer;
